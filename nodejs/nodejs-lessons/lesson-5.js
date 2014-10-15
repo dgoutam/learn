a0dde9c7e70dbf36f7edf5e69de4db497a866de4
Goutam Dey <d_goutam@yahoo.com>
2014-10-15 21:01:17 +0530

    Updated code for better javascript api usage. array.filter().

--- nodejs/nodejs-lessons/lesson-5.js
+++ nodejs/nodejs-lessons/lesson-5.js
@@ -1,27 +1,34 @@
 /**
  * Created by goutamd on 10/15/2014.
  */
-    var fs = require('fs');
-    var path = require('path');
-    var dirArg = process.argv[2];
-    var dirPath;
-    var extension = '.txt'
-    if(dirArg) {
-        dirPath = fileArg;
-    } else {
-        dirPath = './resources/';
-    }
+var fs = require('fs');
+var path = require('path');
+var dirArg = process.argv[2];
+var dirPath;
+var extension = '.txt';
+if (dirArg) {
+    dirPath = fileArg;
+} else {
+    dirPath = './resources/';
+}
 
-    fs.readdir(dirPath,function(err,fileList){
-        //handle error
-        if (err){
-            console.log("Directory ' + dirPath +' does not exists or unable to read from it");
-        }
-        for (i=0; i<fileList.length; i++) {
-            var fileNameWithPath = fileList[i];
-            if(path.extname(fileNameWithPath)=== extension){
-                console.log(fileNameWithPath);
-            }
-        }
+fs.readdir(dirPath, function (err, fileList) {
+    //handle error
+    if (err) {
+        console.log("Directory ' + dirPath +' does not exists or unable to read from it");
+    }
+    fileList.filter(function (file) {
+        return  path.extname(file) === extension;
+    }).forEach(function (file) {
+        console.log(file);
     });
+
+    /*
+     fileList.forEach(function(file) {
+     if (path.extname(file) === extension) {
+     console.log(file.toLocaleString());
+     }
+     });
+     */
+});
 
